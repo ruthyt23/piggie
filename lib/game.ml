b7c67c952bda9ee269a9ada1a5ff7f10e658596e
Nakib <nakibabedin@gmail.com>
2024-07-24 13:42:44 +0000

    add create game from list of names feature

--- lib/game.ml
+++ lib/game.ml
@@ -8,11 +8,31 @@ type t =
   ; open_trades : (int, int * Commodity.t) Hashtbl.t
   }
 
+let create_game_from_names (list_of_player_names : String.t list) =
+  let num_players = List.length list_of_player_names in
+  let players =
+    List.init num_players ~f:(fun idx ->
+      Player.
+        { player_id = idx + 1
+        ; hand = []
+        ; player_name = List.nth_exn list_of_player_names idx
+        })
+  in
+  (* Get all the types of commodities that we are trading and initialize with
+     quantity 9 for each commodity *)
+  let commodities_traded = Hashtbl.create (module Commodity) in
+  let types_of_commodities_traded = Commodity.game_commodities num_players in
+  List.iter types_of_commodities_traded ~f:(fun commodity ->
+    Hashtbl.set commodities_traded ~key:commodity ~data:9);
+  let open_trades = Hashtbl.create (module Int) in
+  { players; commodities_traded; open_trades }
+;;
+
 let create_game num_players =
   (* Number of players is equal to the number of commodites traded *)
   let players =
     List.init num_players ~f:(fun player_id ->
-      Player.{ player_id = player_id + 1; hand = [] })
+      Player.{ player_id = player_id + 1; hand = []; player_name = "" })
   in
   (* Get all the types of commodities that we are trading and initialize with
      quantity 9 for each commodity *)
--- lib/player.ml
+++ lib/player.ml
@@ -4,6 +4,7 @@ open! Fzf
 
 type t =
   { player_id : int
+  ; player_name : string
   ; mutable hand : Commodity.t list
   }
 [@@deriving equal, sexp_of, bin_io]
