9e4b57e03d0d506ac41edab1b2b7047b74dd252a
Nakib <nakibabedin@gmail.com>
2024-07-24 15:14:33 +0000

    add server updates

--- lib/game.ml
+++ lib/game.ml
@@ -43,3 +43,27 @@ let create_game num_players =
   let open_trades = Hashtbl.create (module Int) in
   { players; commodities_traded; open_trades }
 ;;
+
+let get_hand_for_player t target_player_id =
+  let target_player =
+    List.find_exn t.players ~f:(fun player ->
+      Int.equal target_player_id player.player_id)
+  in
+  target_player.hand
+;;
+
+(* cleanup later on *)
+(* Caused an error when we used Game_lib.win_check *)
+let win_checker (player : Player.t) =
+  let first_commodity = List.nth_exn player.hand 0 in
+  List.for_all player.hand ~f:(fun commodity ->
+    Commodity.equal first_commodity commodity)
+;;
+
+let check_for_wins t =
+  let winners =
+    List.filter t.players ~f:(fun player -> win_checker player)
+  in
+  List.map winners ~f:(fun winning_player ->
+    winning_player, List.hd_exn winning_player.hand)
+;;
