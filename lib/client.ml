ecf89b3d708db4c6b6f32171b501ebc0c6d6277f
Ruth Taddesse <ruth.y.taddesse@gmail.com>
2024-07-25 16:34:16 +0000

    client side rpc works!

--- lib/client.ml
+++ lib/client.ml
@@ -7,7 +7,6 @@ let game_id = ref 0
 let player_id = ref 0
 let (hand : Commodity.t list ref) = ref []
 let (book : (Commodity.t * int) list ref) = ref []
-let conn = Option.value_exn !connection
 
 let print_hand hand =
   print_endline "Current hand: ";
@@ -15,7 +14,7 @@ let print_hand hand =
     printf "%s  " (Commodity.to_string commodity))
 ;;
 
-let handle_trade ~commodity ~num_cards =
+let handle_trade ~conn ~commodity ~num_cards =
   let query =
     { Rpcs.Make_trade.Query.player_id = !player_id
     ; Rpcs.Make_trade.Query.game_id = !game_id
@@ -34,13 +33,13 @@ let handle_trade ~commodity ~num_cards =
   return ()
 ;;
 
-let make_trades =
+let make_trades ~conn =
   let num_cards_string_list =
     List.init 4 ~f:(fun num -> Int.to_string (num + 1))
   in
-  printf "\nWhat commodity would you like to trade? ";
+  Core.printf "\nWhat commodity would you like to trade? ";
   let commodity = Stdlib.read_line () |> Commodity.of_string in
-  print_endline "How many would you like to trade? (1-4) ";
+  Core.print_endline "How many would you like to trade? (1-4) ";
   let selection_for_numcards = Pick_from.inputs num_cards_string_list in
   let%bind num_cards_opt =
     pick_one selection_for_numcards ~prompt_at_top:() ~height:7
@@ -50,7 +49,7 @@ let make_trades =
   | Some cards ->
     printf "Number of cards: %s\n" cards;
     let num_cards = Int.of_string cards in
-    handle_trade ~commodity ~num_cards
+    handle_trade ~conn ~commodity ~num_cards
 ;;
 
 let game_over winner_list =
@@ -65,7 +64,7 @@ let game_over winner_list =
 (*else if not (List.equal Commodity.equal !current_hand hand) then
   Core.print_endline "Trade successful!"; current_hand := hand;*)
 
-let pull_game_state =
+let pull_game_state ~conn =
   Deferred.repeat_until_finished () (fun _ ->
     let (query : Rpcs.Game_state.Query.t) = !game_id in
     let%bind (response : Rpcs.Game_state.Response.t) =
@@ -75,7 +74,7 @@ let pull_game_state =
     | Rpcs.Game_state.Response.In_progress ->
       if List.is_empty !hand
       then Core.print_endline "\n*** WELCOME TO PIT! ***";
-      let%bind () = make_trades in
+      let%bind () = make_trades ~conn in
       return (`Repeat ())
     | Rpcs.Game_state.Response.Game_over winner_list ->
       game_over winner_list;
@@ -83,7 +82,7 @@ let pull_game_state =
     | Rpcs.Game_state.Response.Waiting -> return (`Repeat ()))
 ;;
 
-let pull_player_data ~game_id ~player_id =
+let pull_player_data ~conn ~game_id ~player_id =
   Deferred.repeat_until_finished () (fun _ ->
     let (query : Rpcs.Player_game_data.Query.t) =
       { Rpcs.Player_game_data.Query.game_id
@@ -96,8 +95,8 @@ let pull_player_data ~game_id ~player_id =
     let (current_book : (Commodity.t * int) list), player_hand =
       response.current_book, response.player_hand
     in
-    if List.length current_book = List.length !book
-       && List.for_all2_exn
+    if (not (List.length current_book = List.length !book))
+       || List.exists2_exn
             current_book
             !book
             ~f:(fun (commodity1, num1) (commodity2, num2) ->
@@ -106,8 +105,7 @@ let pull_player_data ~game_id ~player_id =
       book := current_book;
       print_endline "*** UPDATED BOOK ***";
       print_s [%sexp (!book : (Commodity.t * int) list)]);
-    if List.length player_hand = List.length !hand
-       && List.equal Commodity.equal player_hand !hand
+    if not (List.equal Commodity.equal player_hand !hand)
     then (
       hand := player_hand;
       print_endline "*** UPDATED HAND ***";
@@ -119,9 +117,17 @@ let book_data =
   Command.async
     ~summary:"Live updating of book data"
     (let%map_open.Command () = return ()
+     and host = flag "-host" (required string) ~doc:"_ host name"
+     and int_port = flag "-port" (required int) ~doc:"_ port to listen on"
      and game = flag "-game" (required int) ~doc:"_ name of player"
      and player = flag "-player" (required int) ~doc:"_ name of player" in
-     fun () -> pull_player_data ~game_id:game ~player_id:player)
+     fun () ->
+       let port = Host_and_port.create ~host ~port:int_port in
+       let%bind conn_bind =
+         Rpc.Connection.client (Tcp.Where_to_connect.of_host_and_port port)
+       in
+       let conn = Result.ok_exn conn_bind in
+       pull_player_data ~conn ~game_id:game ~player_id:player)
 ;;
 
 let connect_to_server =
@@ -149,7 +155,7 @@ let connect_to_server =
          let%bind conn_bind =
            Rpc.Connection.client (Tcp.Where_to_connect.of_host_and_port port)
          in
-         connection := Some (Result.ok_exn conn_bind);
+         let conn = Result.ok_exn conn_bind in
          let%bind (initial_game_data : Rpcs.Waiting_room.Response.t) =
            Rpc.Rpc.dispatch_exn Rpcs.Waiting_room.rpc conn join_game_query
          in
@@ -160,7 +166,7 @@ let connect_to_server =
            host
            !game_id
            !player_id;
-         pull_game_state))
+         pull_game_state ~conn))
 ;;
 
 let command =
