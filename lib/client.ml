1c068d3b4a7eedb82135a5913ca3e2db05493d41
Ruth Taddesse <ruth.y.taddesse@gmail.com>
2024-08-05 21:10:12 +0000

    running game in one terminal - user input not properly working lol

--- lib/client.ml
+++ lib/client.ml
@@ -49,61 +49,6 @@ let handle_trade ~conn ~commodity ~num_cards =
   return ()
 ;;
 
-(* Helper for making trades *)
-let prompt_user_for_commodity () =
-  Core.print_endline "\nWhat commodity would you like to trade? ";
-  Stdlib.read_line () |> Commodity.of_string
-;;
-
-(* Helper for making trades *)
-let prompt_user_for_quantity () =
-  let num_cards_string_list =
-    List.init 4 ~f:(fun num -> Int.to_string (num + 1))
-  in
-  Core.print_endline "How many would you like to trade? (1-4) ";
-  let selection_for_numcards = Pick_from.inputs num_cards_string_list in
-  let%bind num_cards_opt =
-    pick_one selection_for_numcards ~prompt_at_top:() ~height:7
-  in
-  match num_cards_opt |> ok_exn with
-  | None -> failwith "User didn't select a valid value with fuzzy finder"
-  | Some cards -> return (Int.of_string cards)
-;;
-
-let make_trades_loop ~conn =
-  Deferred.repeat_until_finished () (fun _ ->
-    let commodity_opt = prompt_user_for_commodity () in
-    match commodity_opt with
-    | None ->
-      Core.print_endline "Trade rejected, not a valid commodity";
-      return (`Repeat ())
-    | Some commodity ->
-      let quantity = prompt_user_for_quantity () in
-      let%bind num_cards = quantity in
-      let%bind () = handle_trade ~conn ~commodity ~num_cards in
-      return (`Repeat ()))
-;;
-
-let pull_game_state ~conn =
-  Deferred.repeat_until_finished () (fun _ ->
-    let (query : Rpcs.Game_state.Query.t) = !game_id in
-    let%bind conn_bind = conn () in
-    let%bind (response : Rpcs.Game_state.Response.t) =
-      Rpc.Rpc.dispatch_exn Rpcs.Game_state.rpc conn_bind query
-    in
-    match response with
-    | Rpcs.Game_state.Response.In_progress ->
-      if List.is_empty !hand
-      then Core.print_endline "\n*** WELCOME TO PIT! ***";
-      let%bind () = make_trades_loop ~conn in
-      return (`Repeat ())
-    | Rpcs.Game_state.Response.Game_over _ -> return (`Finished ())
-    | Rpcs.Game_state.Response.Waiting ->
-      let span = Time_float.Span.of_ms 250.0 in
-      let%bind () = Async.after span in
-      return (`Repeat ()))
-;;
-
 (* Helper for get_book_and_hand data *)
 let pull_player_data ~(ui : Ui.t) ~conn ~game_id ~player_id =
   let (query : Rpcs.Player_game_data.Query.t) =
@@ -111,9 +56,10 @@ let pull_player_data ~(ui : Ui.t) ~conn ~game_id ~player_id =
     ; Rpcs.Player_game_data.Query.player_id
     }
   in
+  let%bind conn_bind = conn () in
   Rpc.Pipe_rpc.dispatch_iter
     Rpcs.Player_game_data.rpc
-    conn
+    conn_bind
     query
     ~f:(fun response ->
       match response with
@@ -132,6 +78,21 @@ let pull_player_data ~(ui : Ui.t) ~conn ~game_id ~player_id =
            hand := current_hand;
            Ui.update_hand ui current_hand;
            Rpc.Pipe_rpc.Pipe_response.Continue))
+;;
+
+let waiting_room ~conn =
+  Deferred.repeat_until_finished () (fun _ ->
+    let (query : Rpcs.Game_state.Query.t) = !game_id in
+    let%bind conn_bind = conn () in
+    let%bind (response : Rpcs.Game_state.Response.t) =
+      Rpc.Rpc.dispatch_exn Rpcs.Game_state.rpc conn_bind query
+    in
+    match response with
+    | Rpcs.Game_state.Response.Waiting ->
+      let span = Time_float.Span.of_ms 250.0 in
+      let%bind () = Async.after span in
+      return (`Repeat ())
+    | _ -> return (`Finished ()))
 ;;
 
 (* Helper function for connect to server *)
@@ -159,32 +120,6 @@ let initial_dispatch_to_server ~name ~host ~port ~num_players =
   return (initial_game_data, conn)
 ;;
 
-let get_book_and_hand_data =
-  Command.async
-    ~summary:"Live updating of book data"
-    (let%map_open.Command () = return ()
-     and host = flag "-host" (required string) ~doc:"_ host name"
-     and int_port = flag "-port" (required int) ~doc:"_ port to listen on"
-     and game = flag "-game" (required int) ~doc:"_ name of player"
-     and player = flag "-player" (required int) ~doc:"_ name of player" in
-     fun () ->
-       let host_and_port = Host_and_port.create ~host ~port:int_port in
-       (* let ui = Ui.init () in *)
-       let%bind conn_bind =
-         Rpc.Connection.client
-           (Tcp.Where_to_connect.of_host_and_port host_and_port)
-       in
-       let conn = Result.ok_exn conn_bind in
-       let ui = Ui.init () in
-       Ui.update_hand ui !hand;
-       Ui.update_book ui !book;
-       let%bind _ =
-         (* pass in the UI when the time comes *)
-         pull_player_data ~ui ~conn ~game_id:game ~player_id:player
-       in
-       Deferred.never ())
-;;
-
 let connect_to_server =
   Command.async
     ~summary:"Join game"
@@ -210,11 +145,20 @@ let connect_to_server =
          host
          !game_id
          !player_id;
-       pull_game_state ~conn)
+       let%bind _ = waiting_room ~conn in
+       let ui = Ui.init () in
+       Ui.update_hand ui !hand;
+       Ui.update_book ui !book;
+       let%bind _ =
+         (* pass in the UI when the time comes *)
+         pull_player_data ~ui ~conn ~game_id:!game_id ~player_id:!player_id
+       in
+       Deferred.repeat_until_finished () (fun () ->
+         let%bind commodity, num_cards = Ui.manage_user_input ui in
+         let%map _ = handle_trade ~conn ~commodity ~num_cards in
+         `Repeat ()))
 ;;
 
 let command =
-  Command.group
-    ~summary:"Pit Player"
-    [ "join-game", connect_to_server; "book-data", get_book_and_hand_data ]
+  Command.group ~summary:"Pit Player" [ "join-game", connect_to_server ]
 ;;
--- lib/ui.ml
+++ lib/ui.ml
@@ -3,6 +3,32 @@ open! Async
 open! Fzf
 open Curses
 
+let prerr err =
+  match err with
+  | false ->
+    endwin ();
+    failwith "Error!"
+  | true -> ()
+;;
+
+module Input = struct
+  type t =
+    | Up
+    | Down
+    | Escape
+    | Enter
+    | Invalid
+
+  let of_int value =
+    match value with
+    | 65 -> Up
+    | 66 -> Down
+    | 27 -> Escape
+    | 10 -> Enter
+    | _ -> Invalid
+  ;;
+end
+
 module State_manager = struct
   module Current_state = struct
     type t =
@@ -17,6 +43,8 @@ module State_manager = struct
     ; mutable hand_selection_list : Commodity.t list
     ; quantity_selection_list : int list
     ; mutable state : Current_state.t
+    ; mutable curr_commodity_selected : Commodity.t option
+    ; mutable curr_quantity_selected : int option
     }
 
   let init () =
@@ -25,14 +53,46 @@ module State_manager = struct
     ; hand_selection_list = []
     ; quantity_selection_list = [ 1; 2; 3; 4 ]
     ; state = Selecting_Commodity
+    ; curr_commodity_selected = None
+    ; curr_quantity_selected = None
     }
   ;;
 
   let reset t =
     t.state <- Selecting_Commodity;
     t.quantity_cursor <- 0;
-    t.hand_cursor <- 0
+    t.hand_cursor <- 0;
+    t.curr_commodity_selected <- None
   ;;
+
+  let move_up t =
+    match t.state with
+    | Selecting_Commodity ->
+      t.hand_cursor
+      <- (t.hand_cursor - 1) % List.length t.hand_selection_list
+    | Selecting_Quantity -> t.quantity_cursor <- (t.quantity_cursor - 1) % 4
+  ;;
+
+  let move_down t =
+    match t.state with
+    | Selecting_Commodity ->
+      t.hand_cursor
+      <- (t.hand_cursor + 1) % List.length t.hand_selection_list
+    | Selecting_Quantity -> t.quantity_cursor <- (t.quantity_cursor + 1) % 4
+  ;;
+
+  let handle_enter t =
+    match t.state with
+    | Selecting_Commodity ->
+      t.curr_commodity_selected
+      <- List.nth t.hand_selection_list t.hand_cursor;
+      t.state <- Selecting_Quantity
+    | Selecting_Quantity ->
+      t.curr_quantity_selected
+      <- List.nth t.quantity_selection_list t.quantity_cursor
+  ;;
+
+  let handle_escape t = reset t
 end
 
 type t =
@@ -50,14 +110,6 @@ type t =
 let acs_codes = get_acs_codes ()
 let nice_box window = box window acs_codes.vline acs_codes.hline
 let erase_and_box window = wclear window
-
-let prerr err =
-  match err with
-  | false ->
-    endwin ();
-    failwith "Error!"
-  | true -> ()
-;;
 
 let create_hand_window parent_height parent_width =
   let height = parent_height / 2 in
@@ -99,7 +151,10 @@ let reset_commodity_window t =
     t.state_manager, t.commodity_window
   in
   List.iteri state_manager.hand_selection_list ~f:(fun index commodity ->
-    if index = state_manager.hand_cursor
+    if State_manager.Current_state.equal
+         state_manager.state
+         Selecting_Commodity
+       && index = state_manager.hand_cursor
     then wattron commodity_window Curses.A.standout
     else wattroff commodity_window Curses.A.standout;
     mvwaddstr commodity_window !y x (Commodity.to_string commodity) |> prerr;
@@ -166,10 +221,6 @@ let init () : t =
     }
   in
   reset_commodity_window ui;
-  refresh_all_windows ui;
-  timeout 0;
-  let _ = getch () in
-  noecho () |> prerr;
   reset_quantity_window ui;
   refresh_all_windows ui;
   ui
@@ -204,3 +255,41 @@ let update_game_over t message =
 
 (* let enable_trade_input t = timeout 0; ignore (getch ()); wgetstr
    t.trade_parent_window ;; *)
+
+let manage_user_input t =
+  timeout 0;
+  noecho () |> prerr;
+  curs_set 0 |> prerr;
+  Deferred.repeat_until_finished () (fun () ->
+    (match t.state_manager.state with
+     | State_manager.Current_state.Selecting_Commodity ->
+       reset_commodity_window t
+     | State_manager.Current_state.Selecting_Quantity ->
+       reset_quantity_window t);
+    refresh_all_windows t;
+    let user_input = getch () in
+    if not (user_input = -1) then printf "%d " user_input;
+    match Input.of_int user_input with
+    | Up ->
+      print_endline "Up reached";
+      State_manager.move_up t.state_manager;
+      return (`Repeat ())
+    | Down ->
+      print_endline "Down reached";
+      State_manager.move_down t.state_manager;
+      return (`Repeat ())
+    | Escape ->
+      State_manager.handle_escape t.state_manager;
+      return (`Repeat ())
+    | Enter ->
+      print_endline "Enter reached";
+      State_manager.handle_enter t.state_manager;
+      (match
+         ( t.state_manager.curr_commodity_selected
+         , t.state_manager.curr_quantity_selected )
+       with
+       | Some commodity, Some quantity ->
+         return (`Finished (commodity, quantity))
+       | _ -> return (`Repeat ()))
+    | Invalid -> return (`Repeat ()))
+;;
